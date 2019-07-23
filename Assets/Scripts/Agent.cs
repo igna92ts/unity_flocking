afef430234647c9bfc67055ae43f5ea74022bc61
Ignacio Torres <igna_torres92@hotmail.com>
2019-07-22 23:47:23 -0300

    Working

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Agent : MonoBehaviour {
+    Collider2D agentCollider;
+    public bool leader = false;
+    public Collider2D AgentCollider { get { return agentCollider; } }
+    private Vector2 moveVector;
+    public Vector2 dampVelocity;
+
+    void Start() {
+        dampVelocity = Vector2.zero;
+        agentCollider = GetComponent<Collider2D>();
+        moveVector = Vector2.zero;
+    }
+
+    public void UpdateMovement(Flock flock) {
+        if (leader) {
+            moveVector = (Vector2)Input.mousePosition;
+            moveVector = Camera.main.ScreenToWorldPoint(moveVector);
+            moveVector.Normalize();
+        } else {
+            moveVector += flock.CalculateCohesion(this);
+            moveVector += flock.CalculateSeparation(this);
+            moveVector += flock.CalculateAlignment(this);
+            moveVector.Normalize();
+        } 
+        this.Move(moveVector, flock.speedMultiplier);
+    }
+
+    public void Move(Vector2 newDirection, float speed = 1f) {
+        transform.up = newDirection;
+        transform.position += (Vector3)newDirection * Time.deltaTime * speed;
+    }
+}
--- Assets/Scripts/Flock.cs
+++ Assets/Scripts/Flock.cs
@@ -2,64 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Flock : MonoBehaviour
-{
-    public FlockAgent agentPrefab;
-    public List<FlockAgent> agents = new List<FlockAgent>();
-    public FlockBehavior behavior;
+public class Flock : MonoBehaviour {
+    [Range(1, 200)]
+    public int flockCount = 10; 
+    public Agent agentPrefab;
+    public float separationDistance = 5f;
+    public float instanceDensity = 0.1f;
+    private List<Agent> agents = new List<Agent>();
+    public float cohesionWeight = 1f;
+    public float alignmentWeight = 1f;
+    public float separationWeight = 1f;
+    public float agentSmoothTime = .1f;
+    public float speedMultiplier = 1f;
 
-    [Range(10, 500)]
-    public int startingCount = 250;
-    const float AgentDensity = 0.08f;
-    [Range(1f, 100f)]
-    public float driveFactor = 10;
-    [Range(1f, 100f)]
-    public float maxSpeed = 5;
-    [Range(1f, 10f)]
-    public float neighbourRadius = 1.5f;
-    [Range(0f, 1f)]
-    public float avoidanceRadiusMultiplier = 0.5f; 
-
-    float squareMaxSpeed, squareNeighbourRadius, squareAvoidanceRadius;
-    public float SquareAvoidanceRadius { get { return squareAvoidanceRadius; } }
     void Start() {
-       squareMaxSpeed = maxSpeed * maxSpeed; 
-       squareNeighbourRadius = neighbourRadius * neighbourRadius;
-       squareAvoidanceRadius = squareNeighbourRadius * avoidanceRadiusMultiplier * avoidanceRadiusMultiplier;
-
-       for (int i = 0; i < startingCount; i++) {
-            FlockAgent newAgent = Instantiate(
+        for (int i = 0; i < flockCount; i++) {
+            Agent newAgent = Instantiate(
                agentPrefab,
-               Random.insideUnitCircle * startingCount * AgentDensity,
+               Random.insideUnitCircle * flockCount * instanceDensity,
                Quaternion.Euler(Vector3.forward * Random.Range(0f, 360f)),
                transform
             );
             newAgent.name = "Agent" + i;
+            if (i == 0) newAgent.leader = true;
             agents.Add(newAgent);
        }
     }
 
-    void Update() {
-        foreach(FlockAgent agent in agents) {
-            List<Transform> nearbyObjects = GetNearbyObjects(agent);
-            agent.GetComponentInChildren<SpriteRenderer>().color = Color.Lerp(Color.white, Color.red, nearbyObjects.Count / 5f); // TODO mover a variable
-            Vector2 move = behavior.GetMovement(agent, nearbyObjects, this);
-            move *= driveFactor;
-            if (move.sqrMagnitude > squareMaxSpeed) {
-                move = move.normalized * maxSpeed;
-            }
-            agent.Move(move);
+    void LateUpdate() {
+        foreach(Agent agent in agents) {
+            agent.UpdateMovement(this);
         }
     }
 
-    List<Transform> GetNearbyObjects(FlockAgent agent) {
-        List<Transform> nearbyObjects = new List<Transform>();
-        Collider2D[] contextColliders = Physics2D.OverlapCircleAll(agent.transform.position, neighbourRadius);
-        foreach(Collider2D coll in contextColliders) {
-            if (coll != agent.AgentCollider) {
-                nearbyObjects.Add(coll.transform);
+    public Vector2 CalculateCohesion(Agent currentAgent) {
+        Vector2 cohesionVector = Vector2.zero;
+        Vector2 center = Vector2.zero;
+        foreach(Agent agent in agents) {
+            if (agent != currentAgent)
+                center += (Vector2)agent.transform.position;
+        }
+        center /= (agents.Count - 1);
+        cohesionVector = center - (Vector2)currentAgent.transform.position;
+        // cohesionVector.Normalize();
+        return Vector2.SmoothDamp(currentAgent.transform.up, cohesionVector * cohesionWeight, ref currentAgent.dampVelocity, agentSmoothTime);
+        // return cohesionVector * cohesionWeight;
+    }
+
+    public Vector2 CalculateSeparation(Agent currentAgent) {
+        Vector2 separationVector = Vector2.zero;
+        int nAvoid = 0;
+        foreach (Agent agent in agents) {
+            if (agent != currentAgent) {
+                if (Vector2.SqrMagnitude(agent.transform.position - currentAgent.transform.position) < (separationDistance * separationDistance)) {
+                    nAvoid++;
+                    Vector2 headingVector = currentAgent.transform.position - agent.transform.position;
+                    float scale = headingVector.magnitude / separationDistance;
+                    // separationVector += (Vector2)(currentAgent.transform.position - agent.transform.position) / scale;
+                    // headingVector.Normalize();
+                    separationVector += headingVector / scale;
+                }
             }
         }
-        return nearbyObjects;
-    } 
+        if (nAvoid > 0) separationVector /= nAvoid;
+        separationVector.Normalize();
+        return separationVector * separationWeight;
+    }
+
+    public Vector2 CalculateAlignment(Agent currentAgent) {
+        Vector2 alignmentVector = Vector2.zero;
+        foreach (Agent agent in agents) {
+            if (agent != currentAgent) {
+                alignmentVector += (Vector2)agent.transform.up;
+            }
+        }
+        alignmentVector /= (agents.Count - 1);
+        alignmentVector.Normalize();
+        return alignmentVector * alignmentWeight;
+    }
 }
